57bb9983294a64a7e3a2b81a5839dbfe7e729fe6
BerangerSch <beranger.scherpereel@efficientip.com>
2022-11-16 16:21:50 +0100

    Mise en classe Pokemon Api

--- exerciceAPI/pokemon.php
+++ exerciceAPI/pokemon.php
@@ -1,5 +1,7 @@
 <?php
 
+    $urlPokemons = 'http://pokeapi.co/api/v2/pokemon';
+
     class Pokemon{
         public function __construct($url){
             $jsonPokemon = file_get_contents($url);
@@ -12,17 +14,27 @@
 
     class showPokemon{
         public function render($pokeLink){
-            $pokemon = new Pokemon($pokeLink["url"]);
+            $pokemon = new Pokemon($pokeLink);
             echo '<td>' . $pokemon->id . '</td>';
             echo '<td>' . $pokemon->name . '</td>';
             echo '<td><img src=' . $pokemon->img . '></img></td></tr>';
         }
     }
 
-    $listPokemon = file_get_contents('http://pokeapi.co/api/v2/pokemon');
-    $tabPokemon = json_decode($listPokemon, TRUE);
-    $tabPokemon = $tabPokemon['results'];
+    class pokemonAPI{
+        public function __construct($url){
+            $this->listPokemon = file_get_contents($url);
+            $this->tabPokemon = json_decode($this->listPokemon, TRUE);
+            $this->tabPokemon = $this->tabPokemon['results'];
+        }
 
+        public function render(){
+            $show = new showPokemon;
+            foreach($this->tabPokemon as $pokemon){
+                $show->render($pokemon["url"]);
+            }
+        }
+    }
     
 ?>
 
@@ -37,10 +49,8 @@
             </tr>
             <tr>
                 <?php
-                    foreach($tabPokemon as $pokemon){
-                        $show = new showPokemon;
-                        $show->render($pokemon);
-                    }
+                    $pokemons = new pokemonApi($urlPokemons);
+                    $pokemons->render();
                 ?>
         </table>
     </body>
